9f43a073d096e8a5684cc2c97e5a153be88a5570
srhrsb <sebastian.boettcher2@srh.de>
2024-09-03 12:01:28 +0200

    Initial commit

---
+++ src/main/java/com/brh/downloader_2341/Controller.java
@@ -0,0 +1,25 @@
+package com.brh.downloader_2341;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField targetTf;
+
+    @FXML
+    private TextField urlTf;
+
+    @FXML
+    protected void download() {
+
+    }
+
+    @FXML
+    protected void search() {
+
+    }
+
+
+}
